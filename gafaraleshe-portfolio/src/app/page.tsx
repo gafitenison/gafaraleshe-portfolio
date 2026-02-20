103bb12bfe860b78ac3bdaac666c82328e15ea53
Gafar Aleshe <gafaraleshe@Gafars-MacBook-Pro-16.local>
2026-02-20 14:07:42 +0000

    Deploy latest build

--- gafaraleshe-portfolio/src/app/page.tsx
+++ gafaraleshe-portfolio/src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="min-h-screen flex flex-col justify-center px-8">
 
       <h1 className="text-6xl font-bold">
-        Gafar Aleshe
+        Gafar Aleshe Kayode
       </h1>
 
       <p className="mt-4 text-xl text-gray-500">
