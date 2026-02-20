444acf7e4947ef234a81d842fdf78809172f0f9e
Gafar Aleshe <gafaraleshe@Gafars-MacBook-Pro-16.local>
2026-02-20 11:43:58 +0000

    Deploy portfolio

--- gafaraleshe-portfolio/next.config.ts
+++ gafaraleshe-portfolio/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  output: "export", // this enables static HTML export
 };
 
 export default nextConfig;
