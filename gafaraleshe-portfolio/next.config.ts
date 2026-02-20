ce9e3303b5df1b35aee71204b81fd7159df6b9bf
Gafar Aleshe <gafaraleshe@Gafars-MacBook-Pro-16.local>
2026-02-20 12:04:27 +0000

    Deploy portfolio to Hostinger

--- gafaraleshe-portfolio/next.config.ts
+++ gafaraleshe-portfolio/next.config.ts
@@ -1,4 +1,7 @@
-const nextConfig = {
-  output: "export",
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export", // important! makes Next.js generate static HTML
 };
+
 export default nextConfig;
