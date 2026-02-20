61f0df0004d6ebd34ffe99de8b7e3c3d2371acf6
Gafar Aleshe <gafaraleshe@Gafars-MacBook-Pro-16.local>
2026-02-20 12:36:56 +0000

    Add static build for Hostinger

---
+++ gafaraleshe-portfolio/out/_next/static/chunks/0161fc480b131b8c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/500fc61c3eebcba8.js",
+  "static/chunks/ac13cc81989035a1.js",
+  "static/chunks/turbopack-8143bb15d24ed199.js"
+])
---
+++ gafaraleshe-portfolio/out/_next/static/chunks/4b67e0fa10e61b80.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/ad083659dc3456ee.js",
+  "static/chunks/ac13cc81989035a1.js",
+  "static/chunks/turbopack-8261489b710eb0fd.js"
+])
