d9e8358f385a48fc9d0c55e0590fbabd1c11d466
Gafar Aleshe <gafaraleshe@Gafars-MacBook-Pro-16.local>
2026-02-22 00:27:13 +0000

    Deploy 2026-02-22 00:27:13

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@ echo "Building project..."
 npm run build && npm run export
 
 echo "Clearing hostinger-out..."
-rm -rf ../hostinger-out/*
+rm -rf /hostinger-out/*
 
 echo "Copying build files..."
-cp -r out/* ../hostinger-out/
+cp -r out/* /hostinger-out/
 
 echo "Deploying..."
-cd ../hostinger-out
+cd /hostinger-out
 git add .
 git commit -m "Deploy $(date '+%Y-%m-%d %H:%M:%S')"
 git push origin hostinger-out
